3e40020feae4cc233aeae6d89d6a9b0f8cc22f67
JakobBergbomOlsson <jakob567.olsson@gmail.com>
2016-09-09 11:22:39 +0200

    Add files via upload

---
+++ variabler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /*
+            Console.WriteLine("Mata in tal 1: ");
+            string inmatat = Console.ReadLine();
+            int tal1 = int.Parse(inmatat);    
+            Console.WriteLine("Mata in tal 2: ");
+            string inmatat2 = Console.ReadLine();
+            int tal2 = int.Parse(inmatat2);
+            Console.WriteLine("Summa: " + (tal1 + tal2));
+            Console.WriteLine("Produkt: " + (tal1 * tal2));
+            Console.WriteLine("Kvot: " + (tal1 / tal2));
+            */
+
+            Console.WriteLine("Mata in ditt förnamn: ");
+            string inmatat = Console.Readline();
+            string for = string.Parse(inmatat);
+            Console.WriteLine("Mata in ditt efternamn: ");
+            string inmatat2 = Console.ReadLine();
+            string efter = string.Parse(inmatat2);        
+             
+             
+        }
+    }
+}
