9967057a8bb2ae6ea783dd8923515ec15ad4ecf2
JakobBergbomOlsson <jakob567.olsson@gmail.com>
2016-09-23 12:21:54 +0200

    Add files via upload

---
+++ whileloopar.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Övning 1:1
+            /*
+            var n = 0;
+            while (n++<100)
+            {
+                Console.WriteLine(n);
+            }
+            //Övning 2:1
+            int y = 123;
+            int x = 1;
+            while (x != y)
+            {
+                x = int.Parse(Console.ReadLine());
+                if (x != y)
+                {
+                    Console.WriteLine("Fel lösen!");
+                }
+            }
+            Console.WriteLine("Rätt lösen!");
+        }
+        
+            //Övning 1:2
+            var n = 0;
+            while (n++ < 100)
+            {
+                Console.WriteLine(n);
+            }
+            
+            //Övning 2:2
+            var n = 11;
+            while (n-- > 0)
+            {
+                Console.WriteLine(n);
+            }
+            
+            //Övning 3:2
+            while (true)
+            {
+                Console.WriteLine("I'm stuck in a loop!");
+            }
+            
+            Random r = new Random();
+            int tal = r.Next(101);
+            var inmatat = 0;
+            var x = 0;
+            while (tal != inmatat)
+            {
+                inmatat = int.Parse(Console.ReadLine());
+                if ( tal != inmatat)
+                {
+                    Console.WriteLine("Fel! Gissa igen");
+                    x++;
+                }
+                
+            }
+            Console.WriteLine("Rätt gissning!");
+            Console.WriteLine("Antal Gissningar: " + x);
+            
+            int x = 0;
+            int y = 0;
+            int z = 0;
+            int max = 1000000;
+            while (x <= max)
+            {
+                x = z + y;
+                z = x + y;
+                y++;
+                Console.WriteLine(x);
+                
+            }
+            //Övning 5:2
+            int x = 0; int y = 1;
+            while (y <= 1000000)
+            {
+                Console.Write(" " +y);
+                x = x + y;
+                Console.Write(" " +x);
+                y = x + y;
+            }
+      
+            //Övning 6:2
+            Console.Write("Ange saldo på konto <kr>: ");    double start = double.Parse(Console.ReadLine());
+            Console.Write("Ange ränta <%>: ");              double r = double.Parse(Console.ReadLine());
+            Console.Write("Ange mål <kr>: ");               int goal = int.Parse(Console.ReadLine());
+            int years = -1;
+            while (start <= goal)
+            {
+
+                years++;
+                start = start * (1+ (r/100));
+                
+
+            }
+            Console.WriteLine("Det tog " + years + "år innan saldot nådde " + start);
+            }
+            
+            var x = 2016;
+            while (x < 2050)
+            {
+                Console.WriteLine("Det kommer vara skottår år:" + x);
+                x = x + 4;
+            }
+            */
+
+        }
+    }
+}
+
